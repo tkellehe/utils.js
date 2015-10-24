9058f82d8e1b03522bb25e7c32846871b216ebe0
tkellehe <mitchell.kelleher@gmail.com>
2015-10-23 22:15:15 -0400

    Created nullptr.js
    In the attempt to make nullptr more useful to other tools besides
    iter.js, nullptr.js was created.

---
+++ nullptr.js/nullptr.js
@@ -0,0 +1,18 @@
+;(function(_, _defineProperty, _freeze) {
+/**
+ * Used to handle an empty object that always returns NaN for valueOf
+ * as well as make easy checking for whether or not something isFinite
+ * a nullptr.
+ */
+function nullptr() {
+	var me = this;
+	// Makes sure that the valueOf always returns NaN.
+	_defineProperty(me, 'valueOf', {value:function(){return NaN}});
+};
+
+// The current version of nullptr.js.
+_defineProperty(nullptr, 'VERSION', {value:'1.0.0'});
+
+// Freezes an instance of nullptr.
+_defineProperty(_, 'nullptr', {value:_freeze(new nullptr())});
+}).call(this, this, Object.defineProperty, Object.freeze)
