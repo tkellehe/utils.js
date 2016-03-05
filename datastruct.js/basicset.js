f1c20257a324a6533e1d35390420fbb8e9978fd0
tkellehe <mitchell.kelleher@gmail.com>
2016-03-05 15:33:13 -0500

    Set to BasicSet
    Created BasicSet that uses the newer design for classes.
    This set is designed currently to take in just strings and numbers.
    Also, there is no way to remove anything...
    Note: The datastructs do not need to provide plugin capabilities!

---
+++ datastruct.js/basicset.js
@@ -0,0 +1,47 @@
+(function(global, g_defhidden, g_defprop) {
+
+/**
+ * var s = new BasicSet();
+ * 
+ * Every item added will be given and index to the BasicSet.
+ * This allows users to loop through all of the entries.
+ * 
+ * Adding a string/number that has already been added will be ignored.
+ * 
+ * Also, currently only provides adding to the set.
+ * 
+ * When adding, the index where the element was added will be returned.
+ */
+function BasicSet() {
+    var _members = {
+        self       : this,
+        __length__ : 0,
+        __map__    : {}
+    };
+
+    g_defprop(self, "add", {value:function(elem){
+        if(!_members.__map__.hasOwnProperty(elem))
+        {
+            _members.__map__[elem] = _members.__length__++;
+            g_defprop(_members.self, _members.__map__[elem], elem);
+        }
+        return _members.__map__[elem];
+    }});
+}
+
+g_defhidden(BasicSet, "__version__", "1.0.0")
+
+g_defprop(global, "BasicSet", BasicSet);
+
+})(this,
+    Object.defineProperty ?
+    function(object, prop, value) { Object.defineProperty(object, prop, {value:value}) }
+    :
+    function(object, prop, value) { object[prop] = value }
+,
+    Object.defineProperty ?
+    function(object, prop, value) { Object.defineProperty(object, prop, {value:value, enumerable:true}) }
+    :
+    function(object, prop, value) { object[prop] = value }
+
+);
