831b3b0a7bfc0f0e803f81c45fdf6ddf76d0e331
tkellehe <mitchell.kelleher@gmail.com>
2016-03-06 00:08:22 -0500

    SimpleArray: remove and bug fix
    Left is_function in the check for the forloop. Changed it to f
    instanceof Function instead since is currently the only place that
    checks to see if a function.
    
    Added the ability to remove indexes of the SimplaArry.

--- datastruct.js/simplearray.js
+++ datastruct.js/simplearray.js
@@ -10,14 +10,21 @@ function SimpleArray() {
     };
     g_defhidden(_members.__self__, "push", function(element) {
         _members.__self__.resize();
-        _members.__self__[""+_members.length++] = element;
+        _members.__self__[_members.length++] = element;
+        return _members.__self__;
+    });g_defhidden(_members.__self__, "remove", function(i) {
+        if(typeof i === "number" && 0 <= i && i <= _members.length)
+        {
+            delete _members.__self__[i];
+            _members.__self__.resize();
+        }
         return _members.__self__;
     });
     g_defhidden(_members.__self__, "resize", function() {
         var counter = 0;
         for(var i = 0; i < _members.length; ++i) {
             if((""+i) in _members.__self__) {
-                _members.__self__[""+counter++] = _members.__self__[i];
+                _members.__self__[counter++] = _members.__self__[i];
             }
         }
         // Clean up the offset.
@@ -40,7 +47,7 @@ function SimpleArray() {
  * @param f : A function to be called "length" times.
  */
 g_defprop(SimpleArray.prototype, "forloop", function(f) {
-    if(is_function(f)) {
+    if(f instanceof Function) {
         this.resize();
         var index = 0;
         while(index < this.length()) f(index, this[index++], this);
