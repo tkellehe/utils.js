56c02f68206f7e286ae1d58d3cd4ad7c54e11b33
tkellehe <mitchell.kelleher@gmail.com>
2016-04-28 06:24:31 -0400

    Removed ifs
    Got rid of ifs that only added branches. This should help with the speed.

--- polyfill.js/set_timeout.js
+++ polyfill.js/set_timeout.js
@@ -2,9 +2,9 @@
 
 pfill.defprop(pfill, "set_timeout", function set_timeout(f, params, delay, scope){
     if(!(pfill.is_function(f))) return -1;
-    if(!(pfill.is_object(scope))) scope = this;
-    if(!(pfill.is_value(delay))) delay = 0;
-    if(!(pfill.is_array(params))) params = [];
+    ((pfill.is_object(scope)) || (scope = this));
+    ((pfill.is_value(delay)) || (delay = 0));
+    ((pfill.is_array(params)) || (params = []));
     return setTimeout(+delay, function() {
         try { f.apply(scope, params) } catch(e) { if(!(e instanceof pfill.exception)) throw e; }
     });
